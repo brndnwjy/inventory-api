c20ea4ad1072e930231c71bbe32b13811315cabf
Brandon Wijaya <brndnwjy8@gmail.com>
2023-01-08 06:59:15 +0700

    Add cors, helmet and xss-clean for security

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 require("dotenv").config();
 
+// server
 const express = require("express");
 const app = express();
+
+// import packages
+const cors = require("cors");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+
+// security
+app.use(cors());
+app.use(helmet());
+app.use(xss());
 
 const PORT = process.env.PORT || 4000;
 
