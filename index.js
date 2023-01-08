fe54717b6fb6787f131c4a266a87d413111dae17
Brandon Wijaya <brndnwjy8@gmail.com>
2023-01-08 07:05:14 +0700

    Add body-parser and morgan

--- index.js
+++ index.js
@@ -5,14 +5,27 @@ const express = require("express");
 const app = express();
 
 // import packages
+const bp = require("body-parser");
 const cors = require("cors");
-const helmet = require("helmet");
 const xss = require("xss-clean");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+// parser
+app.use(bp.json());
+app.use(
+  bp.urlencoded({
+    extended: true,
+  })
+);
 
 // security
 app.use(cors());
 app.use(helmet());
 app.use(xss());
+
+// logger
+app.use(morgan("dev"));
 
 const PORT = process.env.PORT || 4000;
 
